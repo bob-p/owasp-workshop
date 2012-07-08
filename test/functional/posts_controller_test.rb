fe5d6f18de004a46367b8a47438204081cf41201
Thomas Pomfret <thomaspomfret@gmail.com>
2012-07-08 12:31:44 +0100

    Updated tests for security fixes

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -8,46 +8,86 @@ class PostsControllerTest < ActionController::TestCase
       sign_in @user
     end
 
-    should "should get index" do
+    should "get index" do
       get :index
       assert_response :success
-      assert_not_nil assigns(:posts)
+      assert_equal [@post], assigns(:posts)
     end
 
-    should "should get new" do
+    should "get new" do
       get :new
       assert_response :success
     end
 
-    should "should create post" do
-      assert_difference('@user.posts.count') do
-        post :create, post: { title: 'hello', body: 'body'}
+    should "create post" do
+      assert_difference('Post.count') do
+        post :create, post: { title: 'hello', body: 'body' }
       end
 
       assert_redirected_to post_path(assigns(:post))
     end
 
-    should "should show post" do
+    should "show post" do
       get :show, id: @post
       assert_response :success
     end
-
-    should "should get edit" do
+    
+    should "get edit" do
       get :edit, id: @post
       assert_response :success
     end
 
-    should "should update post" do
+    should "update post" do
       put :update, id: @post, post: { title: 'hello', body: 'body' }
       assert_redirected_to post_path(assigns(:post))
     end
 
-    should "should destroy post" do
+    should "destroy post" do
       assert_difference('Post.count', -1) do
         delete :destroy, id: @post
       end
 
       assert_redirected_to posts_path
+    end
+
+    context 'viewing anothers posts' do
+      setup { post_two = posts(:two) }
+    
+      should "not show others post" do
+        assert_raise ActiveRecord::RecordNotFound do
+          get :show, id: 2
+        end
+      end
+
+      should "not list others posts" do
+        get :index, :user_id => 2
+        assert_equal [@post], assigns(:posts)
+        assert_response :success
+      end
+
+      should "not edit anothers post" do
+        assert_raise ActiveRecord::RecordNotFound do
+          get :edit, :id => 2
+        end
+      end
+
+      should "not update anothers post" do
+        assert_raise ActiveRecord::RecordNotFound do
+          put :update, :id => 2
+        end
+      end
+      
+      should "not delete anothers post" do
+        assert_raise ActiveRecord::RecordNotFound do
+          delete :destroy, :id => 2
+        end
+      end
+
+      should "not create a post for another user" do
+        assert_raise ActiveModel::MassAssignmentSecurity::Error do
+          post :create, post: { title: 'hello', body: 'body', user_id: 2 }
+        end
+      end
     end
   end
 
@@ -78,13 +118,13 @@ class PostsControllerTest < ActionController::TestCase
     
     should "not post create" do
       assert_no_difference 'Post.count' do
-        post :create, post: { title: 'hello', body: 'body', user_id: 1 } 
+        post :create, post: { title: 'hello', body: 'body' } 
       end
       assert_redirected_to new_user_session_path
     end
     
     should "should not update post" do
-      put :update, id: @post, post: { title: 'hello', body: 'body', user_id: 1 }
+      put :update, id: @post, post: { title: 'hello', body: 'body' }
       assert_redirected_to new_user_session_path
     end
 
